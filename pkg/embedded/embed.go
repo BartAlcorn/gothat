2ad003afea774c57cdd78328d9566bfba8025186
Bart Alcorn <bart.alcorn@wbd.com>
2024-12-02 06:37:57 -0500

    feat: cleanup, misnamed script

--- pkg/embedded/embed.go
+++ pkg/embedded/embed.go
@@ -16,7 +16,7 @@ var Scripts = []RemoteFile{
 	{Folder: "htmx", Name: "htmx@2.0.3.min.js", URL: "https://unpkg.com/htmx.org@2.0.3/dist/htmx.min.js"},
 	{Folder: "htmx", Name: "htmx-ext-sse@2.2.2.js", URL: "https://unpkg.com/htmx-ext-sse@2.2.2/sse.js"},
 	{Folder: "htmx", Name: "htmx-ext-alpine-morph@2.0.0.js", URL: "https://unpkg.com/htmx-ext-alpine-morph@2.0.0/alpine-morph.js"},
-	{Folder: "htmx", Name: "htmx-ext-sseinclude-vals@2.0.0.js", URL: "https://unpkg.com/htmx-ext-include-vals@2.0.0/include-vals.js"},
+	{Folder: "htmx", Name: "htmx-ext-include-vals@2.0.0.js", URL: "https://unpkg.com/htmx-ext-include-vals@2.0.0/include-vals.js"},
 	// {Folder: "htmx", Name: "hyperscript.org@0.9.12.js", URL: "https://unpkg.com/hyperscript.org@0.9.12"},
 	{Folder: "alpinejs", Name: "ui@3.14.5.min.js", URL: "https://unpkg.com/@alpinejs/ui@3.14.5/dist/cdn.min.js"},
 	{Folder: "alpinejs", Name: "anchor@3.14.5.min.js", URL: "https://unpkg.com/@alpinejs/anchor@3.14.5/dist/cdn.min.js"},
